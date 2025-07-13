99b82858da3f07a565e9421677dd6e9275880742
nikhililandhirai <nikilan486@gmail.com>
2025-07-13 22:07:47 +0530

    workdone

---
+++ lesson19/activity3.js
@@ -0,0 +1,23 @@
+<html>
+    <body>
+        <h2>
+            JavaScript statements
+        </h2>
+        <p>
+            A
+            <b>
+                jawascript program is list of statements
+            </b>
+            to be extended by the computer
+        </p>
+        <p id="demo"></p>
+        <script>
+            variable x,y,z
+            x=5
+            y=6
+            z=x+y
+            document. getElementByid("demo").innerHTML =
+            "The value of z is" +z+".";
+        </script>
+    </body>
+</html>
