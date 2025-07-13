0566084ddbdf23e3cd5a9658f59768a824c814e4
nikhililandhirai <nikilan486@gmail.com>
2025-07-13 21:58:42 +0530

    workdone

---
+++ lesson19/activity2.js
@@ -0,0 +1,14 @@
+<html>
+    <body>
+        <h1>
+            java script variable
+        </h1>
+        <p id="result"></p>
+        <script>
+            variable num1= 5;
+            variable num2= 6;
+            variable num1+num2
+document.getElementByid("result").innerHTML ="The total sum is:" =sum
+        </script>
+    </body>
+</html>
---
+++ lesson19/actyvity1.js
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <h1 id="myH"></h1>
+        <p id="myP"></p>
+        <script>
+            // My Heading:
+            document.getElementBy("myH").innerHTML="JavaScript Comments";
+            //my paragraph:
+            document.getElementBy("myP").innerHTML="my first paragraph";
+        </script>
+    </body>
+</html>
